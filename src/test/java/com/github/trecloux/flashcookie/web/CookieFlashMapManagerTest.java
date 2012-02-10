262df16fc9766c848119710df8b4240bb9437e4f
Thomas Recloux <trecloux@commerce-btoc.com>
2012-02-10 11:42:50 +0100

    Green : encode and decode a complex type

--- src/test/java/com/github/trecloux/flashcookie/web/CookieFlashMapManagerTest.java
+++ src/test/java/com/github/trecloux/flashcookie/web/CookieFlashMapManagerTest.java
@@ -54,28 +54,6 @@ public class CookieFlashMapManagerTest {
 		assertEquals(new Integer(11), subComplexValue.getNumber());
 	}
 	
-	public class TestComplexType {
-		String str;
-		Integer number;
-		TestComplexType complex;
-		public String getStr() {
-			return str;
-		}
-		public void setStr(String str) {
-			this.str = str;
-		}
-		public Integer getNumber() {
-			return number;
-		}
-		public void setNumber(Integer number) {
-			this.number = number;
-		}
-		public TestComplexType getComplex() {
-			return complex;
-		}
-		public void setComplex(TestComplexType complex) {
-			this.complex = complex;
-		}
-	}	
+	
 	
 }
---
+++ src/test/java/com/github/trecloux/flashcookie/web/TestComplexType.java
@@ -0,0 +1,25 @@
+package com.github.trecloux.flashcookie.web;
+
+public class TestComplexType {
+	String str;
+	Integer number;
+	TestComplexType complex;
+	public String getStr() {
+		return str;
+	}
+	public void setStr(String str) {
+		this.str = str;
+	}
+	public Integer getNumber() {
+		return number;
+	}
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+	public TestComplexType getComplex() {
+		return complex;
+	}
+	public void setComplex(TestComplexType complex) {
+		this.complex = complex;
+	}
+}	
